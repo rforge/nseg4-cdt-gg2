0acb413802763e1ebac10b72f6b6c7ddd5a74fc5
sarahheaps <sarahheaps@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-09-19 16:18:55 +0000

    Added source.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nseg4-cdt-gg2@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/cdtRggplot2-package.R
@@ -0,0 +1,11 @@
+#' R courses for Newcastle University's CDT in Cloud 
+#' Computing for Big Data
+#' 
+#' Functions and datasets used for Newcastle University's 
+#' CDT in Cloud Computing for Big Data. This package is 
+#' used in the "Advanced Graphics" course.
+#' @name cdtRggplot2-package
+#' @aliases cdtRggplot2
+#' @docType package
+#' @keywords package
+NULL
